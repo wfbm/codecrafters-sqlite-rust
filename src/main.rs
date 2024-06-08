8424a8221813572774336adc4b5296a9b8aa20ae
wfbm <william.balan.machado@gmail.com>
2024-06-07 23:56:00 -0300

    3rd stage

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ fn main() -> Result<()> {
         _ => {}
     }
 
-    // Parse command and act accordingly
     let command = &args[2];
     match command.as_str() {
         ".dbinfo" => {
@@ -20,14 +19,35 @@ fn main() -> Result<()> {
             file.read_exact(&mut header)?;
 
             let page_size = u16::from_be_bytes([header[16], header[17]]);
-            println!("database page size: {}", page_size);
-
             let mut first_page = vec![0; page_size as usize];
-
             file.read_exact(&mut first_page)?;
+
             let table_count = u16::from_be_bytes([first_page[3], first_page[4]]);
 
+            println!("database page size: {}", page_size);
             println!("number of tables: {}", table_count);
+        }
+        ".table" => {
+            let mut file = File::open(&args[1])?;
+            let mut header = [0; 100];
+            file.read_exact(&mut header)?;
+
+            let page_size = u16::from_be_bytes([header[16], header[17]]);
+
+            let mut first_page = vec![0; page_size as usize];
+            file.read_exact(&mut first_page)?;
+
+            let pointer = u16::from_be_bytes([first_page[5], first_page[6]]);
+            let pointer = pointer - 100;
+
+            let content = &first_page[pointer as usize..page_size as usize];
+            let commands = String::from_utf8_lossy(&content);
+
+            let re = regex::Regex::new(r"CREATE TABLE (?P<table_name>[a-zA-Z_-]+)").unwrap();
+
+            for table_name in re.captures_iter(&commands) {
+                println!("{:?}", &table_name["table_name"]);
+            }
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
